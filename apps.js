1cbfa2f86f1aa7aa1bbdc239d4884d25159f6984
ZoneTwelve <zonetwelve159@gmail.com>
2017-04-29 12:45:56 +0800

    test 3d mode

--- apps.js
+++ apps.js
@@ -1,7 +1,15 @@
+var arr = [];
+for(var i=0;i<100;i++)
+	arr.push(i)
+arr.sort(function(){return .5-Math.random()});
+
 function setup(){
-    createCanvas(window.innerWidth*.8 ,window.innerHeight*.9);
+    createCanvas(window.innerWidth*.8 ,window.innerHeight ,WEBGL);
 }
 
 function draw(){
-	background(51);
+	rotateX(.03);
+	rotateY(frameCount*0.005);
+	for(var i=0;i<arr.length;i++)
+		box(width/arr.length ,height/100*i ,arr[i]);
 }
